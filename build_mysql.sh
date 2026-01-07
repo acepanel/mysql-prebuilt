6ad43a0b9a93bf0b01928afac13745edeac51a38
耗子 <haozi@loli.email>
2026-01-07 21:01:26 +0800

    fix: build

--- build_mysql.sh
+++ build_mysql.sh
@@ -33,6 +33,7 @@ sed -i '1i\RETURN()' plugin/x/CMakeLists.txt
 sed -i '1i\RETURN()' extra/protobuf/CMakeLists.txt
 sed -i '1i\RETURN()' plugin/group_replication/CMakeLists.txt
 sed -i '/^ADD_SUBDIRECTORY(protobuf)/d' sql/CMakeLists.txt
+sed -i '/^TARGET_LINK_LIBRARIES(sql_main ext::libprotobuf-lite)/d' sql/CMakeLists.txt
 
 # 编译
 mkdir build
